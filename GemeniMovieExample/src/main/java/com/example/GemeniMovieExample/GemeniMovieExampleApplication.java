13f3efac35f96a59d74e02ced1a64e19a9d769f1
DanzillaDanny <thenumberonesonsband@gmail.com>
2026-02-06 00:27:14 -0600

    set up

--- GemeniMovieExample/src/main/java/com/example/GemeniMovieExample/GemeniMovieExampleApplication.java
+++ GemeniMovieExample/src/main/java/com/example/GemeniMovieExample/GemeniMovieExampleApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GemeniMovieExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GemeniMovieExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GemeniMovieExampleApplication.class, args);
+    }
 
-
+}
 
