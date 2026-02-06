a1e0f2c6a9dfb8657ae7a3f1390796f7f0aaa30c
DanzillaDanny <thenumberonesonsband@gmail.com>
2026-02-06 12:48:02 -0600

    changes

--- GemeniMovieExample/src/main/java/com/example/GeminiMovieExample/models/Movie.java
+++ GemeniMovieExample/src/main/java/com/example/GeminiMovieExample/models/Movie.java
@@ -1,20 +1,22 @@
 package com.example.GeminiMovieExample.models;
 
+import com.google.genai.types.GenerateContentResponse;
+
 public class Movie {
     private String title;
     private int rating;
     private String description;
-    private String genre;
-    private String director;
+    private String cast;
+    private String averageRating;
 
-    public Movie(String title, int rating, String description, String genre, String director) {
-        this.title = "";
-        this.rating = 0;
-        this.description = "";
-        this.genre = "";
-        this.director = "";
+    String newline = System.lineSeparator();
 
-
+    public Movie(String title, int rating, String description, String cast, String averageRating) {
+        this.title = title;
+        this.rating = rating;
+        this.description = description;
+        this.cast = cast;
+        this.averageRating = averageRating;
     }
 
     public String getTitle() {
@@ -24,32 +26,45 @@ public class Movie {
     public void setTitle(String title) {
         this.title = title;
     }
+
     public int getRating() {
         return rating;
     }
+
     public void setRating(int rating) {
+        this.rating = rating;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
-    public String getGenre() {
-        return genre;
-    }
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-    public String getDirector() {
-        return director;}
-}
 
-@Override
-public String toString() {
-    return
-            "Title: " + title + newline +
-                    "Rating: " + rating + newline;
-    "Description: " + description + newline;
+    public String getCast() {
+        return cast;
+    }
+
+    public void setCast(String cast) {
+        this.cast = cast;
+    }
+
+    public String getAverageRating() {
+        return averageRating;
+    }
+
+    public void setAverageRating(String averageRating) {
+        this.averageRating = averageRating;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Title: " + title + newline +
+                        "Rating: " + rating + newline +
+                        "Description: " + description + newline;
+    }
 }
 
